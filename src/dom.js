16f5be847dd219b41c27322911b72ee046093dbb
yatharth <yatharthmakvana@gmail.com>
2025-07-26 18:28:05 +0530

    Wrote the functioning of the delete button on the project name.

--- src/dom.js
+++ src/dom.js
@@ -2,15 +2,47 @@ export function Domstuff(){
 
     const add=(tag1,value)=>{
         const parent=document.querySelector(tag1);
-        const child=document.createElement("div");
-        child.classList.add("projectName");
+        const child=document.createElement("button");
         const deleteButton=document.createElement("button");
         deleteButton.textContent="del";
+        deleteButton.classList.add("delete");
+        deleteButton.addEventListener("click", (e) => {
+            e.stopPropagation();
+            parent.removeChild(child);
+        });
         child.textContent=`${value}`;
         child.append(deleteButton);
         parent.append(child);
     }
 
-    return{ add};
+    const addTodo=(tag, value1,value2,value3,value4)=>{
+        const parent=document.querySelector(tag);
+        const box=document.createElement("div");
+        const title=document.createElement("div");
+        title.textContent=`${value1}`;
+
+        const description=document.createElement("div");
+        description.textContent=`${value2}`;
+
+        const date=document.createElement("div");
+        date.textContent=`${value3}`;
+
+        const priority=document.createElement("div");
+        priority.textContent=`${value4}`;
+
+        const stat=document.createElement("checkbox");
+        
+        box.appendChild(title);
+        box.appendChild(description);
+        box.appendChild(date);
+        box.appendChild(priority);
+        box.appendChild(stat);
+
+        parent.appendChild(box);
+
+    }
+        
+
+    return{ add,addTodo};
 
 }
--- src/toDo.js
+++ src/toDo.js
@@ -1,3 +1,5 @@
+import {Domstuff} from "./dom";
+
 export function ToDo(){
     
     class Todo{
@@ -10,12 +12,25 @@ export function ToDo(){
     }
     }
 
+    const dom=Domstuff();
+
     const takeTodoInput=()=>{
-        
+        const todoForm=document.getElementById("myformtodo");
+
+        todoForm.addEventListener("submit",(e)=>{
+            e.preventDefault();
+            const title=document.getElementById("title").value;
+            const description=document.getElementById("description").value;
+            const dueDate=document.getElementById("dueDate").value;
+            const priority=document.getElementById("priority").value;
+
+            const task=new Todo(title,description,dueDate,priority,"checked");
+            printTodo(task);
+        })
     }
 
-    const createObject=()=>{
-        
+    const printTodo=(task)=>{
+        dom.addTodo(".todoList",task.title,task.description,task.dueDate,task.priority);
     }
 
     
